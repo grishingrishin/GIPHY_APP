c41fa9a7d208989a3352238215a5f8295e80e575
ygorkagrishin <grishingrishin@yahoo.com>
2020-08-08 17:16:39 +0300

    Delete component PageSection and rename comoponents AllTrends ->Trends

--- src/components/Trends.vue
+++ src/components/Trends.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: "AllTrands"
+  name: "Trends"
 }
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,49 @@
 <template>
   <div class="homepage">
-    <PageSection title="Trending GIFs">
-      <AllTrands/>
-    </PageSection>
-    <PageSection title="Artists">
-      <Artists/>
-    </PageSection>
-    <PageSection title="All Stories">
-      <Stories/>
-    </PageSection>
+    <section class="section trending">
+      <div class="container">
+        <div class="section__header trending__header">
+          <h2 class="section__title trending__title">
+            Trending GIFs
+          </h2>
+          <a href="/" class="section__all trending__all">See All</a>
+        </div>
+        <Trends/>
+      </div>
+    </section>
+    <section class="section artists">
+      <div class="container">
+        <div class="section__header artists__header">
+          <h2 class="section__title artists__title">
+            Artists
+          </h2>
+          <a href="/" class="section__all trending__all">See All Artists</a>
+        </div>
+        <Artists/>
+      </div>
+    </section>
+    <section class="section stroies">
+      <div class="container">
+        <div class="section__header stroies__header">
+          <h2 class="section__title stroies__title">
+            All Stories
+          </h2>
+        </div>
+        <Stories/>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import PageSection from "../components/PageSection.vue";
-import AllTrands from "../sections/AllTrands.vue";
-import Artists from "../sections/Artists.vue";
-import Stories from "../sections/Stories.vue";
+import Trends from "../components/Trends.vue";
+import Artists from "../components/Artists.vue";
+import Stories from "../components/Stories.vue";
 
 export default {
   name: "Home",
   components: {
-    PageSection,
-    AllTrands,
+    Trends,
     Artists,
     Stories,
   }
